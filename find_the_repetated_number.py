00ae9324a88b14943543c6e5104682b08370f49f
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-07-29 12:37:16 +0530

    changed from math.ceil to math.floor

--- find_the_repetated_number.py
+++ find_the_repetated_number.py
@@ -7,7 +7,7 @@ n = int(n)
 A = list(map(int,input().split()))
 a = Counter(A)
 most_occured_one = a.most_common(1)
-if most_occured_one[0][1] > math.ceil(n/3):
+if most_occured_one[0][1] > math.floor(n/3):
     print(most_occured_one[0][0])
 else:
     print(-1)
