27b6a2f0564f075459ad21bdf4bd438f0c089fdf
Pulavarti vinay kumar <56190203+111801034@users.noreply.github.com>
2020-07-29 12:17:35 +0530

    Add files via upload
    Google tech-interview question

---
+++ find_the_repetated_number.py
@@ -0,0 +1,13 @@
+
+from collections import Counter
+import math
+
+n = input()
+n = int(n)
+A = list(map(int,input().split()))
+a = Counter(A)
+most_occured_one = a.most_common(1)
+if most_occured_one[0][1] > math.ceil(n/3):
+    print(most_occured_one[0][0])
+else:
+    print(-1)
